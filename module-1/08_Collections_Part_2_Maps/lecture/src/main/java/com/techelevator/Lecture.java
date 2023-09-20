0e8f24513a0994fe58e73346cca23b8aa8aa084f
Walt Impellicceiri <walt@techelevator.com>
2023-09-20 08:52:10 -0400

    New material

---
+++ module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,39 @@
+package com.techelevator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+
+		System.out.println("####################");
+		System.out.println("        MAPS");
+		System.out.println("####################");
+		System.out.println();
+
+
+		System.out.println("####################");
+		System.out.println("        SETS");
+		System.out.println("####################");
+		System.out.println();
+	}
+
+	/*
+		Problems:
+
+		1) Determine if a list has duplicates.
+
+		2) Given a list, remove all duplicates from it.
+
+		3) Build a structure that represents a dictionary of words and their definitions.
+
+		4) Given a list of integers, find and return a number that appears most frequently.
+		In the case of a tie, return the largest value.
+
+		5) Given two Strings, return true if they are anagrams.
+
+	 */
+
+}
