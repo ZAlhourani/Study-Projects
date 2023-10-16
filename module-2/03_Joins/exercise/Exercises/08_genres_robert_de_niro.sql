b566854e8feb117866cf6ba5f2364611a2bd28e7
Abd Alazeez (Zee) Alhourani <iz.emad@gmail.com>
2023-10-16 13:04:59 -0400

    insert exercise

--- module-2/03_Joins/exercise/Exercises/08_genres_robert_de_niro.sql
+++ module-2/03_Joins/exercise/Exercises/08_genres_robert_de_niro.sql
@@ -1,13 +1,25 @@
 -- 8. The genres of movies that Robert De Niro has appeared in that were released in 2010 or later, sorted alphabetically.
 -- (6 rows)
-select genre_name
-from person
-join movie_actor on movie_actor.actor_id = person_id
-join movie on movie.movie_id = movie_actor.movie_id
-join movie_genre on movie.movie_id = movie_genre.movie_id
-join genre on movie_genre.genre_id = genre.genre_id
+
+-- first solution :
+
+--select genre_name
+--from person
+--join movie_actor on movie_actor.actor_id = person_id
+--join movie on movie.movie_id = movie_actor.movie_id
+--join movie_genre on movie.movie_id = movie_genre.movie_id
+--join genre on movie_genre.genre_id = genre.genre_id
+--where person_name = 'Robert De Niro' and release_date >= '2010-01-01'
+--group by genre_name
+--order by genre_name asc;
+
+-- walt solution :
+
+select distinct genre_name
+from genre
+join movie_genre using (genre_id)
+join movie using (movie_id)
+join movie_actor using(movie_id)
+join person on person_id = actor_id
 where person_name = 'Robert De Niro' and release_date >= '2010-01-01'
-group by genre_name
-order by genre_name asc;
-
-
+order by genre_name;
--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
@@ -1,3 +1,8 @@
 -- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person
 --     table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
+insert into person (person_name, birthday)
+values ('Zee Alhourani', '1989-05-25');
 
+update movie
+set director_id = (select person_id from person where person_name = 'Zee Alhourani')
+where title = 'The Blob';
