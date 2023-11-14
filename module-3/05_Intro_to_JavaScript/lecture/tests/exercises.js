fd77a997b0d391e1f82b1845882a9d6baa84d59f
Abd Alazeez (Zee) Alhourani <iz.emad@gmail.com>
2023-11-13 20:26:14 -0500

    03a CSS Grid exercise

--- module-3/05_Intro_to_JavaScript/lecture/tests/exercises.js
+++ module-3/05_Intro_to_JavaScript/lecture/tests/exercises.js
@@ -6,3 +6,35 @@
 function nonStart(word1, word2) {
   return word1.substring(1) + word2.substring(1);
 }
+
+function excludeLength4Words(words) {
+  const result = [];
+
+  for (const word of words) {
+
+    if (word.length !==4){
+      result.push(word);
+    }
+  }
+  return result;
+} 
+
+function returnSumOfLargestAndSmallestNumber(nums) {
+
+  if (nums == null || nums.length === 0) {
+    return null;
+  }
+
+  let smallest = nums[0];
+  let largest = nums[0];
+  
+  for(let i = 1; i < nums.length; i++) {
+    if (nums[i] < smallest) {
+      smallest = nums[i];
+    }
+    if (nums[i] > largest) {
+      largest = nums[i];
+    }
+  }
+  return smallest + largest;
+}
