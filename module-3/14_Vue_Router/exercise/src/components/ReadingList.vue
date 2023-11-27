085af4d3878e42c6e01f8df8632a2440dc2f246e
Walt Impellicceiri <walt@techelevator.com>
2023-11-27 12:38:58 -0500

    New material

---
+++ module-3/14_Vue_Router/exercise/src/components/ReadingList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="book-container">
+    <router-link to="">
+      <book-card v-bind:book="book" v-for="book in $store.state.books" v-bind:key="book.isbn" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import BookCard from '../components/BookCard.vue';
+
+export default {
+  components: {
+    BookCard
+  }
+}
+</script>
+
+<style>
+.book-container {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+</style>
--- module-3/14_Vue_Router/lecture/src/router/index.js
+++ module-3/14_Vue_Router/lecture/src/router/index.js
@@ -1,7 +1,30 @@
 import { createRouter as _createRouter, createWebHistory } from 'vue-router';
 
-const routes = [
+import AddReviewView from '@/views/AddReviewView.vue';
+import BlahBlah from '@/views/BlahBlah.vue';
+import ProductsView from '@/views/ProductsView.vue';
+import ProductDetails from '@/views/ProductDetails.vue';
 
+const routes = [
+  {
+    name: 'home-page',
+    path: '/',
+    component: ProductsView
+  },
+  {
+    name: 'product-details',
+    path: '/products/:id',
+    component: ProductDetails
+  },
+  {
+    name: 'add-review',
+    path: '/products/:id/reviews/create',
+    component: AddReviewView
+  },
+  {
+    path: '/blah',
+    component: BlahBlah
+  }
 ];
 
 export function createRouter () {
